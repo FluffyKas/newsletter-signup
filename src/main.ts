b5da4ced63a86fc9d9538cf894bece2dc77d7df3
Zsófia Maruzs <zsofia@splendex.io>
2023-06-23 11:17:57 +0200

    add success page

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,38 @@
 const email = document.getElementById('email') as HTMLInputElement;
 const errorMsg = document.getElementById('error-msg') as HTMLElement;
 const submitBtn = document.getElementById('submit-btn') as HTMLButtonElement;
-const regex = /^[w.-]+@[w.-]+.w+$/;
-console.log('CICA');
-console.log(email, submitBtn);
+const subscriptionPage = document.getElementById('subscription-page') as HTMLElement;
+const successPage = document.getElementById('success-page') as HTMLElement;
+const successEMail = document.getElementById('success-email') as HTMLElement;
+const dismissBtn = document.getElementById('dismiss-btn') as HTMLButtonElement;
+const regex = /^\w+@\w+\.\w+$/;
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  if (!email.value) {
+    errorMsg.innerText = 'Email cannot be empty';
+  } else if (!regex.test(email.value)) {
+    errorMsg.innerText = 'Valid email required';
+  }
+
   if (regex.test(email.value)) {
-    console.log(regex.test(email.value));
-    email.classList.remove('error');
-    errorMsg.classList.add('hidden');
+    subscriptionPage.classList.add('hidden');
+    successPage.classList.remove('hidden');
+    successEMail.innerText = email.value;
   } else {
     e.preventDefault();
     email.classList.add('error');
     errorMsg.classList.remove('hidden');
   }
+});
+
+dismissBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  successPage.classList.add('hidden');
+  subscriptionPage.classList.remove('hidden');
+});
+
+email.addEventListener('input', () => {
+  email.classList.remove('error');
+  errorMsg.classList.add('hidden');
 });
